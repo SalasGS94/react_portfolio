f7a40a54e604a991686642c9a5d8ece8d651fe4b
Gerardo Garcia-Salas <gergasa94@gmail.com>
2023-08-13 09:13:05 -0600

    functional react app

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div id="About-Me" class="About-Me">
+      <h2>About Me</h2>
+      <p>
+        I am a physician invested in continuing education. I intend to pursue a further honing in my current skills
+      </p>
+    </div>
+  );
+}
